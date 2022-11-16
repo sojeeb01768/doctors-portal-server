9a9e4835208e6755b1a8f2ae2caa81cf5d2a964d
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-16 19:08:45 +0600

    booking query

--- index.js
+++ index.js
@@ -21,9 +21,25 @@ async function run() {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions')
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
 
+        // use Aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            console.log(date);
             const quiry = {};
             const options = await appointmentOptionCollection.find(quiry).toArray();
+
+            // get the booking of the provided date
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+            // code carefully
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot)
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+                option.slots = remainingSlots;
+                console.log(date, option.name, remainingSlots.length);
+            })
             res.send(options);
         });
 
@@ -39,6 +55,19 @@ async function run() {
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email:booking.email,
+                treatment: booking.treatment
+            }
+
+            const alreadyBooking = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooking.length) {
+                const message = `you already have a booking on ${booking.appointmentDate}`
+                return res.send({acknowledged: false, message})
+            }
+
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
