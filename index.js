f868c0b6c9bd5b1f6e7aa4f6e0dcf9fd2455062d
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-11-23 19:34:54 +0600

    final

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ async function run() {
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
         const usersCollection = client.db('doctorsPortal').collection('users');
         const doctorsCollection = client.db('doctorsPortal').collection('doctors');
+        const paymentsCollection = client.db('doctorsPortal').collection('payments');
 
         //NOTE:  make sure you use varifyAdmin after verifyJWT
         const verifyAdmin = async (req, res, next) => {
@@ -166,6 +167,22 @@ async function run() {
                 clientSecret: paymentIntent.client_secret,
             });
         });
+
+        // post payment collection to database
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = paymentsCollection.insertOne(payment);
+            const id = payment.bookingId;
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
 
 
         // jwt token generate
